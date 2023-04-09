4555fb974f4a89b3c059c489590dcef720f1b0de
Stephen Oba <obastepheno@gmail.com>
2023-04-09 21:46:39 +0100

    ➕ Add method minOperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module contains solution to min operations question
+"""
+
+
+def minOperations(n):
+    """
+    method that calculates the fewest number of
+    operations needed to result in exactly `n` `H`
+    characters in the file.
+
+    Args
+    ----
+    n(int): Number of times to call the operations
+
+    Returns
+    -------
+    An integer
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
