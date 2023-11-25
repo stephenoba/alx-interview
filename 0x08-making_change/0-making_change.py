eb1f311fe4bdf93c6685cc8cb1f02edbf96e8d85
Stephen Oba <obastepheno@gmail.com>
2023-11-25 19:19:01 +0100

    Update file 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,16 +8,19 @@ def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet
     a give total amount.
     """
-    if total == 0:
+    if total <= 0:
         return 0
-    # Initialize the dp array
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Fill the dp array
-    for c in coins:
-        for i in range(c, total + 1):
-            dp[i] = min(dp[i], dp[i - c] + 1)
-
-    # Check if the total can be reached
-    return -1 if dp[total] == float('inf') else dp[total]
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
